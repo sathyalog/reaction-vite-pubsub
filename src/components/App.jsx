4f60bebc89e896679938f1bba966f8194b149d90
Sathya Vakacharla <sathyavakacharla@192.168.1.140>
2022-12-30 17:52:44 +0000

    pubsub configured and integrated with app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,45 @@
-import { useReducer } from 'react';
-import reducer, {initialState} from '../state/reducer';
-import MessageBoard from './MessageBoard';
-import PublishMessage from './PublishMessages';
-import AppContext from '../state/appContext';
+import { useReducer, useEffect } from "react";
+import reducer, { initialState } from "../state/reducer";
+import PubSub from "../pubsub";
+import MessageBoard from "./MessageBoard";
+import PublishMessage from "./PublishMessages";
+import AppContext from "../state/appContext";
 
+const pubsub = new PubSub();
+
+setTimeout(() => {
+  pubsub.publish({
+    type: "foo",
+    value: "bar",
+  });
+}, 1000);
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const providerState = {
-    state,
-    dispatch
-  }
+
+  useEffect(() => {
+    pubsub.addListener({
+      message: (messageObject) => {
+        const { channel, message } = messageObject;
+        console.log("Received message", channel, message);
+        dispatch(message);
+      },
+    });
+  }, []);
+
+  // const providerState = {
+  //   state,
+  //   dispatch,
+  // };
   return (
-    <AppContext.Provider value={{state, dispatch}}>
-      
-       <h2>Reaction</h2>
-       <hr/>
-       <PublishMessage />
-       <hr />
-       <MessageBoard />
-    
+    <AppContext.Provider value={{ state, dispatch }}>
+      <h2>Reaction</h2>
+      <hr />
+      <PublishMessage />
+      <hr />
+      <MessageBoard />
     </AppContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
